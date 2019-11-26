8c8f284979943c195c10146a84c161bfb45cf245
209347 <209347@edu.p.lodz.pl>
2019-11-26 18:06:25 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.net.http.HttpRequest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "Revese me please";
+
+    }
+}
